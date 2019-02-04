8dbcc72f128b63d9577a5185375b1321dc047c57
Al Mamun Khan <almamunkhan04@gmail.com>
2019-02-04 18:28:53 +0600

    added multi thread

---
+++ camera.py
@@ -0,0 +1,43 @@
+from threading import Thread, Lock
+import cv2
+
+
+class CameraStream(object):
+    def __init__(self, src=0):
+        self.stream = cv2.VideoCapture(src)
+
+        (self.grabbed, self.frame) = self.stream.read()
+        self.started = False
+        self.read_lock = Lock()
+
+    def start(self):
+        if self.started:
+            print("already started!!")
+            (self.grabbed, self.frame) = self.stream.read()
+
+        self.started = True
+        self.thread = Thread(target=self.update, args=())
+        self.thread.start()
+        return self
+
+
+    def update(self):
+        while self.started:
+            (grabbed, frame) = self.stream.read()
+            self.read_lock.acquire()
+            self.grabbed, self.frame = grabbed, frame
+            self.read_lock.release()
+
+
+    def read(self):
+        self.read_lock.acquire()
+        frame = self.frame.copy()
+        self.read_lock.release()
+        return frame
+
+    def stop(self):
+        self.started = False
+        self.thread.join()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stream.release()
